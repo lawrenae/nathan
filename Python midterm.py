d7e366b7d873758e1b4c164b3cb9120f43c97855
Lawrence, Andrew <andrew.lawrence@kroger.com>
2019-10-24 14:30:16 -0400

    refactor

--- Python midterm.py
+++ Python midterm.py
@@ -1,89 +1,59 @@
-#choices for classes and races
+def pick_one(title, choices):
+    print("|Select a %s." % title)
+    for choice in choices:
+        print()
+        print(choice)
 
-Race_Choices = ['Human', 'Half-Elf', 'Elf', 'Dwarf', 'Half-Orc', 'Halfing', 'Tiefling', 'Dragonborn', 'Gnome']
-Class_Choices = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
+    choice = input("Enter a %s name. " % title)
+    print("You have selected", choice)
+    print()
+    return choice
+
+def get_roll(current):
+    return int(input("Enter the numbers for your %s dice roll. " % current))
 
 
-Race_Choices.sort(key=len)
-Class_Choices.sort(key=len)
+def rolling_stats():
+    too_big = 24 #dont like magic numbers
+    max_role = too_big
+
+    while (max_role >= too_big):
+        roll_1 = get_roll("1st")
+        roll_2 = get_roll("2nd")
+        roll_3 = get_roll("3rd")
+        roll_4 = get_roll("4th")
+        roll_5 = get_roll("5th")
+        roll_6 = get_roll("6th")
+        
+        max_role = max(roll_1, roll_2, roll_3, roll_4, roll_5, roll_6)
+
+        if max_role > too_big:
+            print("No rolls can be higher than %i." % too_big)
+            print("Retrying...")
+
+    return max_role
+	
+def roll_section(title):
+    print()
+    print("ROLL FOR %s:" % title)
+    print('-' * len(title))
+    print("Your highest roll is %d " % rolling_stats())
+
 
 firstname = input('Please enter the first name of your character. ')
 lastnmae = input('Please enter the last name of your character. ')
 print()
 
-print("|Select a Race.")
-for race in Race_Choices:
-	print()
-	print(race)
+#choices for classes and races
+Race_Choices = sorted(['Human', 'Half-Elf', 'Elf', 'Dwarf', 'Half-Orc', 'Halfing', 'Tiefling', 'Dragonborn', 'Gnome'], key=len)
+Class_Choices = sorted(['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard'], key=len)
+pick_one("Race", Race_Choices)
+pick_one("Class", Class_Choices)
 
-Race = input("Enter a Race name. ")
-print("You have selected", Race)
-print()
-
-print("|Select a Class.")
-for klass in Class_Choices:
-	print()
-	print(klass)
-
-Klass = input('Enter a Class name. ')
-print("you have selected", Klass)
-
-
-def get_roll(current):
-    roll = int(input("Enter the numbers for your %s dice roll. " % current))
-    if roll > 24:
-        print("Number can't be higher than 24.")
-
-    return roll
-
-
-def rolling_stats():
-        too_big = 500 #dont like magic numbers
-        max_role = too_big
-
-        while (max_role > 24):
-            roll_1 = get_roll("1st")
-            roll_2 = get_roll("2nd")
-            roll_3 = get_roll("3rd")
-            roll_4 = get_roll("4th")
-            roll_5 = get_roll("5th")
-            roll_6 = get_roll("6th")
-        
-            max_role = max(roll_1, roll_2, roll_3, roll_4, roll_5, roll_6)
-
-            if max_role > too_big:
-                print("Retrying...")
-
-        return max_role
 	
-	
-print()
-print("ROLL FOR STRENGTH:")
-print(18 * "-")
-print("Your highest roll is %d " % rolling_stats())
-
-print()
-print("ROLL FOR DEXTERITY:")
-print(20 * "-")
-print("Your highest roll is %d " % rolling_stats())
-
-print()
-print("ROLL FOR CONSTITUTION:")
-print(25 * "-")
-print("Your highest roll is %d " % rolling_stats())
-
-print()
-print("ROLL FOR INTELLIGENCE")
-print(25 * "-")
-print("Your highest roll is %d " % rolling_stats())
-
-print()
-print("ROLL FOR WISDOM:")
-print(15 * "-")
-print("Your highest roll is %d " % rolling_stats())
-
-print()
-print("ROLL FOR CHARISMA:")
-print(20 * "-")
-print("Your highest roll is %d " % rolling_stats())
-
+roll_section("STRENGTH")
+roll_section("DEXTERITY")
+roll_section("CONSTITUTION")
+roll_section("INTELLIGENCE")
+roll_section("WISDOM")
+roll_section("CHARISMA")
